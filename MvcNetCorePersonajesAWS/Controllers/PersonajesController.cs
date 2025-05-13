148bb9095a8f9607e99ecd049f1fc646f72f0e3e
Danirolan21 <danirolan21@hotmail.com>
2025-05-13 14:13:04 +0200

    Add project files.

---
+++ MvcNetCorePersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCorePersonajesAWS.Models;
+using MvcNetCorePersonajesAWS.Services;
+
+namespace MvcNetCorePersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServiceApiPersonajes service;
+        public PersonajesController(ServiceApiPersonajes service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajes();
+            return View(personajes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Personaje personaje)
+        {
+            await this.service.CreatePersonajesAsync(personaje.Nombre, personaje.Imagen);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcNetCorePersonajesAWS/Services/ServiceApiPersonajes.cs
@@ -0,0 +1,64 @@
+﻿using MvcNetCorePersonajesAWS.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcNetCorePersonajesAWS.Services
+{
+    public class ServiceApiPersonajes
+    {
+        private MediaTypeWithQualityHeaderValue header;
+        private string UrlApi;
+
+        public ServiceApiPersonajes(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiPersonajes");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Personaje>> GetPersonajes()
+        {
+            using (HttpClientHandler handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    string request = "api/personajes";
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(this.header);
+                    HttpResponseMessage response = await client.GetAsync(this.UrlApi + request);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        List<Personaje> personajes = await response.Content.ReadAsAsync<List<Personaje>>();
+                        return personajes;
+                    }
+                    else
+                        return null;
+                }
+            }
+        }
+
+        public async Task CreatePersonajesAsync(string nombre, string imagen)
+        {
+            using (HttpClientHandler handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    string request = "api/personajes";
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(this.header);
+                    Personaje personaje = new Personaje
+                    {
+                        IdPersonaje = 0,
+                        Nombre = nombre,
+                        Imagen = imagen
+                    };
+                    string json = JsonConvert.SerializeObject(personaje);
+                    StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(this.UrlApi + request, content);
+                }
+            }
+        }
+    }
+}
